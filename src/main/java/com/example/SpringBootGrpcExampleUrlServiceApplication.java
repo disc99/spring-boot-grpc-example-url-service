868e6ffe2ad2101718c9ea22ff96ba52ee5ec066
d.kanehira <disc99.g@gmail.com>
2016-07-27 20:48:51 +0900

    Impimente grpc service

--- src/main/java/com/example/SpringBootGrpcExampleUrlServiceApplication.java
+++ src/main/java/com/example/SpringBootGrpcExampleUrlServiceApplication.java
@@ -1,5 +1,9 @@
 package com.example;
 
+import io.example.url.Url;
+import io.example.url.UrlServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import org.lognet.springboot.grpc.GRpcService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +12,22 @@ public class SpringBootGrpcExampleUrlServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootGrpcExampleUrlServiceApplication.class, args);
+	}
+
+	@GRpcService
+	public static class UrlService extends UrlServiceGrpc.UrlServiceImplBase {
+		@Override
+		public void shortcut(Url.ShortcutRequest request, StreamObserver<Url.ShortcutResponse> responseObserver) {
+			Url.ShortcutResponse res = Url.ShortcutResponse.newBuilder().setShortUrl("short").build();
+			responseObserver.onNext(res);
+			responseObserver.onCompleted();
+		}
+
+		@Override
+		public void parse(Url.ParseRequest request, StreamObserver<Url.ParseResponse> responseObserver) {
+			Url.ParseResponse res = Url.ParseResponse.newBuilder().setBaseUrl("https://google.com").build();
+			responseObserver.onNext(res);
+			responseObserver.onCompleted();
+		}
 	}
 }
